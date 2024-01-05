e58a972e9670fc4643e09fa49ca5d478138a9b4d
daszhugh@gmail.com <daszhugh>
2024-01-05 16:11:39 +0800

    Fix blas

--- SuiteSparse_config/cmake_modules/SuiteSparsePolicy.cmake
+++ SuiteSparse_config/cmake_modules/SuiteSparsePolicy.cmake
@@ -112,9 +112,9 @@ if ( WIN32 )
     add_compile_definitions ( _CRT_SECURE_NO_WARNINGS )
 endif ( )
 
-if ( BLAS_UNDERSCORE )
+if ( SUITESPARSE_BLAS_UNDERSCORE )
     add_compile_definitions ( BLAS_UNDERSCORE )
-endif( BLAS_UNDERSCORE )
+endif( SUITESPARSE_BLAS_UNDERSCORE )
 
 enable_language ( C )
 include ( GNUInstallDirs )
